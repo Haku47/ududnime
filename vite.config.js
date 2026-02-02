0025420906410dc1edc9e5b7def3b9f7f8039b69
unknown <ryunsk47@gmail.com>
2026-02-03 04:54:42 +0700

    fix: add path alias to vite config for vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // Tambahkan ini gais
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // Ini bagian gahar yang bikin @ merujuk ke folder src gais
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
