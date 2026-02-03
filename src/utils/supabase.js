b4a2b3c6a7526255a435bdd77f6c78818e92ca70
unknown <ryunsk47@gmail.com>
2026-02-03 08:29:53 +0700

    update: aw here we go again back it. x2

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAK = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAK)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
