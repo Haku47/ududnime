dd5dc54347dac43d2592387f17ddb62df45002db
unknown <ryunsk47@gmail.com>
2026-02-03 19:59:58 +0700

    new: Some big updates due to testing android, and yes etc.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import path from 'path' // Tambahkan ini gais
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: './' memastikan semua aset (CSS/JS) terbaca benar oleh Capacitor Android gais!
+  base: './', 
+  
   plugins: [vue()],
+  
   resolve: {
     alias: {
       // Ini bagian gahar yang bikin @ merujuk ke folder src gais
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  // Optimasi Build gais agar folder dist selalu bersih
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
